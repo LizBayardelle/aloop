62b5747168d58130d9dd39cdb9cab1640a76224c
Liz Bayardelle <lizbayardelle@gmail.com>
2019-11-06 09:29:07 -0800

    Multiple Recipient Test

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,6 +3,7 @@ class ContactMailer < ApplicationMailer
 
 	def new_contact_us_submission(contact)
 		@contact = contact
-		mail(to: "lizbayardelle@gmail.com", subject: "Contact Us Submission from Aloop.com")
+		mail(to: "lizbayardelle@gmail.com, liz@thestaysanemom.com", subject: "Contact Us Submission from Aloop.com")
 	end
 end
+# pete@aloop.com, sales@aloop.com, lisa@aloop.com
