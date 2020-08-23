4e30a178b9bc670ef03aeabff02d548036cbd077
Liz Bayardelle <lizbayardelle@gmail.com>
2020-08-22 18:35:28 -0700

    Instagram Added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @latest_blog = Blog.where(published: true).order("created_at DESC").limit(1).first
+    @videos = Blog.where.not(video_url: "").limit(12)
   end
 
   def info
